8288b0374eddbda408f7ae6861ef75498fcfface
sierra-hub <kteta.sierra@gmail.com>
2020-10-04 13:58:55 +0200

    added a propmpt to the development image to click and show a paragraph

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("#developmentimage").click(function(){
+      $("#developmentimage").slideDown('1500').hide('1000');
+      $("#development").show('1500');
+    });
+    $("#development").click(function(){
+      $("#development").slideUp('1500');
+      $("#developmentimage").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#designimage").click(function(){
+      $("#designimage").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#designimage").slideDown('1500');
+    });
+  });
