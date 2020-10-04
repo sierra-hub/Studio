c87deebf8c57d932b61b1b04f7baeebc4acfb4f5
sierra-hub <kteta.sierra@gmail.com>
2020-10-04 14:05:18 +0200

    added a propmpt to click on image and name of image

--- js/script.js
+++ js/script.js
@@ -40,3 +40,55 @@ $(document).ready(function(){
       $("#product-image").slideDown('1500');
     });
   });
+  $(document).ready(function(){
+    $("#black").mouseover(function(){
+      $("#overlay").show();
+    }).mouseout(function(){
+      $("#overlay").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#ontario").mouseover(function(){
+      $("#overlay2").show();
+    }).mouseout(function(){
+      $("#overlay2").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#pyramids").mouseover(function(){
+      $("#overlay3").show();
+    }).mouseout(function(){
+      $("#overlay3").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#jim").mouseover(function(){
+      $("#overlay4").show();
+    }).mouseout(function(){
+      $("#overlay4").hide();
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#have").mouseover(function(){
+      $("#overlay5").show();
+    }).mouseout(function(){
+      $("#overlay5").hide();
+    });
+    $("#calculator").mouseover(function(){
+      $("#overlay6").show();
+    }).mouseout(function(){
+      $("#overlay6").hide();
+    });
+    $("#burned").mouseover(function(){
+      $("#overlay7").show();
+    }).mouseout(function(){
+      $("#overlay7").hide();
+    });
+    $("#giraffe").mouseover(function(){
+      $("#overlay8").show();
+    }).mouseout(function(){
+      $("#overlay8").hide();
+    });
+  });
+  
