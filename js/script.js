b0634fc3ebc068dab606b048e6363514651c6491
sierra-hub <kteta.sierra@gmail.com>
2020-10-04 14:08:15 +0200

    added a prompt of the submit button

--- js/script.js
+++ js/script.js
@@ -91,4 +91,19 @@ $(document).ready(function(){
       $("#overlay8").hide();
     });
   });
+  $(document).ready(function(){
+    $("form#formcontact").submit(function(event){
+      var name = $("input#MERGE1").val();
+      var email = $("input#MERGE0").val();
+      var message = $("textarea#comment").val();
+      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+        alert (name + ", we have received your message. Thank you for contacting us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+      
+    });
+  
+  });
   
